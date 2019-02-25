4e36c249917e5144a4962ebc6a205f1cbc685b55
Jason P. Leasure <jpleasu@gmail.com>
2019-02-25 14:35:13 +0000

    study: add Rep, update notes

--- study/py2_coroutines.py
+++ study/py2_coroutines.py
@@ -4,7 +4,20 @@ use coroutines to sample
 
 TODO
 ====
-- can a tracetree record the choices made?
+- should a tracetree record the choices made?
+  - repeating an Alt node is choosing the same child
+  - repeating a Rep node is choosing the same _number_ of repetitions
+  - "nonempty(child)" gfunc?
+    - no entropy, just sampling. If its children are fixed, it can return a
+      constant (its original result), o/w is must be run again.
+  - "randint()" gfunc?
+    - repeating its behavior would be just returning the same string.. no gexpr
+      args
+  - "newvar(id)" and "randvar()" gfuncs?
+    - newvar populates state, so it must be run
+    - randvar randomly selects an existing state, so state must be prescribed
+      and it must get the same choice.
+
 - do constrained sampling
   - tree constraint from a tracetree
   - global parameter setting, where nodes of the expression tree get parameters
@@ -12,6 +25,19 @@ TODO
 - composition?
   - produce a tracetree from a constrained sample result
 
+  - mkgen returns a coroutine that will ask for samples by yielding GExprs. We
+    can compose mkgens with functional composition, interpreting GExprs as we go.
+
+      def mkgen1(x,ge):
+        if isinstance(ge,Alt):
+          # do something interesting
+        return default_mkgen(x,ge)
+
+    the sampler is always calling the same mkgen.. although we could have
+    progress through a tracetree..
+
+  - compile to coroutines..
+  - analyze coroutines to avoid 'yield' keyword within list comphrehensions
 '''
 from __future__ import absolute_import, division, print_function
 import sys
@@ -48,72 +74,17 @@ class Cat(GExpr):
     self.children=l
 
 
+class Rep(GExpr):
+  def __init__(self, sub, mi, ma):
+    self.sub=sub
+    self.mi=mi
+    self.ma=ma
+
 class Rule(GExpr):
   def __init__(self, rhs=None):
     self.rhs=rhs
   def set(self,rhs):
     self.rhs=rhs
-
-def mkgen(ge):
-  if isinstance(ge,Tok):
-    def g(x):
-      yield ge.s
-  elif isinstance(ge,Alt):
-    def g(x):
-      s=yield x.random.choice(ge.children)
-      yield s
-  elif isinstance(ge,Cat):
-    def g(x):
-      s=''
-      for cge in ge.children:
-        s+=yield cge
-      yield s
-  elif isinstance(ge,Rule):
-    def g(x):
-      s=yield(ge.rhs)
-      yield s
-  else:
-    raise Exception('unrecognized GExpr type: %s' % type(ge))
-  return g
-
-
-def sample(x,ge):
-  'recursive sampler'
-
-  g=mkgen(ge)(x)
-  r=next(g)
-  while not isinstance(r,str):
-    r=g.send(sample(x,r))
-  return r
-
-def itorstr(x,ge):
-  print('||| %s |||' % ge)
-  if isinstance(ge,GExpr):
-    return mkgen(ge)(x)
-  else:
-    return ge
-
-def stk_sample(x,ge):
-  '''
-    a sampler that doesn't rely on recursion
-  '''
-  stack=[itorstr(x,ge)]
-  while True:
-    if isinstance(stack[-1],str):
-      # stack looks like
-      #   caller     <- parent generator, awaiting result
-      #     callee   <- child generator, now finished
-      #       result <- its string result
-      #
-      #  so send the result into caller, and push caller's next
-      s=stack.pop()
-      if len(stack)==1:
-        return s
-      stack[-1]=itorstr(x,stack[-2].send(s))
-    else:
-      it=stack[-1]
-      stack.append(itorstr(x,next(it)))
-
 
 class TTNode(object):
   __slots__='ge','parent','children','s'
@@ -145,9 +116,11 @@ class Sampler(object):
   def itorstr(self,ge):
     if isinstance(ge,GExpr):
       # is a child of one up (or root)
+      # enter
       return self.mkgen(ge)
     else:
       # fed to two up
+      # exit
       return ge
 
   def sample(self,ge):
@@ -179,6 +152,14 @@ class Sampler(object):
       def g():
         s=yield(ge.rhs)
         yield s
+    elif isinstance(ge,Rep):
+      def g():
+        s=''
+        n=self.x.random.randint(ge.mi,ge.ma+1)
+        while n>0:
+          s+=yield ge.sub
+          n-=1
+        yield s
     else:
       raise Exception('unrecognized GExpr type: %s' % type(ge))
     return g()
@@ -186,45 +167,30 @@ class Sampler(object):
 class TraceTreeVisitorSampler(Sampler):
   def reset(self):
     super().reset()
-    self.tt=[]
     self.tt_root=None
+    self.tt=None
 
   def itorstr(self,ge):
     if isinstance(ge,GExpr):
       # is a child of one up (or root)
       n=TTNode(ge)
-      if len(self.tt)==0:
+      if self.tt==None:
         self.tt_root=n
       else:
-        n.set_parent(self.tt[-1])
-      self.tt.append(n)
+        n.set_parent(self.tt)
+      self.tt=n
 
       return self.mkgen(ge)
     else:
-      n=self.tt.pop()
-      n.s=ge
+      self.tt.s=ge
+      self.tt=self.tt.parent
       # fed to two up
       return ge
 
 
-def basic_test():
-  r=Rule()
-  r.set(Alt(Tok(">"), Cat(Tok("."),r)))
-  
-  #ge=Alt(Tok('asdf'),Tok('1234'))
-  
-  ge=r
-  
-  X=namedtuple('X','random state')
-  x=X(np.random.RandomState(),type('',(),{}))
-  
-  for i in range(10):
-    #print(sample(x,ge))
-    print(stk_sample(x,ge))
-
 if __name__=='__main__':
   r=Rule()
-  r.set(Alt(Tok(">"), Cat(Tok("."),r)))
+  r.set(Alt(Rep(Tok(">"),1,3), Cat(Tok("."),r)))
 
   ge=r
 
