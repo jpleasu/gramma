3e9e79241a107435e38655edc7b8e68a581f7b82
Jason P. Leasure <jpleasu@gmail.com>
2020-06-16 17:15:11 -0400

    add using_def example

---
+++ examples/using_def.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''
+
+using the def gfunc
+
+'''
+
+from __future__ import absolute_import, division, print_function
+
+from gramma import *
+
+class UsingDef(GrammaGrammar):
+    G=r'''
+        start := r;
+
+        r:=def('x',`x+1`).(
+              `print(x) or x/(1.+x)` "a" | "b".r 
+        ).def('x',`x-1`);
+
+    '''
+
+    def __init__(x):
+        GrammaGrammar.__init__(x,type(x).G, sideeffects=[DepthTracker])
+
+    def reset_state(self,state):
+        state.x=0
+
+
+if __name__=='__main__':
+    print(GrammaSampler(UsingDef()).sample())
+
+
+# vim: ts=4 sw=4
